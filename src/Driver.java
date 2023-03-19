caa2b17476eb2757343e0a2803dd51408cef98dc
Anock20 <119288779+Anock20@users.noreply.github.com>
2023-03-19 19:38:11 +0900

    Initial commit

---
+++ src/Driver.java
@@ -0,0 +1,6 @@
+public interface Driver {
+    public String getName();
+    public void setName(String type);
+    public String addVehicle();
+    public void printVehicle(String color);
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,8 @@
+public interface Vehicle {
+    public String getType();
+    public void setType(String type);
+    public String getColor();
+    public void setColor(String color);
+}
+
+
